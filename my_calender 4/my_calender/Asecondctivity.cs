1f44edd07d32f8782f0c1e5c3da829f677d136fa
zehua <guozehua1996@gmail.com>
2018-08-12 14:06:13 +1000

    version5.0

---
+++ my_calender 4/my_calender/Asecondctivity.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+//using System;
+
+namespace my_calender
+{
+    [Activity(Label = "Asecondctivity")]
+    public class Asecondctivity : Activity
+    {
+        Button btnCalculate;
+        EditText etTotal;
+        TextView tvTipAmount;
+        TextView tvTotal;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.secondlayout);
+
+            btnCalculate = FindViewById<Button>(Resource.Id.btnSubmit);
+
+            etTotal = FindViewById<EditText>(Resource.Id.inputBill);
+
+            tvTipAmount = FindViewById<TextView>(Resource.Id.tipAmount);
+
+            tvTotal = FindViewById<TextView>(Resource.Id.totalAmount);
+
+            btnCalculate.Click += CalculateTip;
+
+
+            void CalculateTip(object sender, EventArgs e)
+            {
+                double total = double.Parse(etTotal.Text);
+
+                double tip = total * 0.15;
+
+                tvTotal.Text = (total + tip).ToString();
+
+                tvTipAmount.Text = tip.ToString();
+
+            } 
+        }
+ 
+    }
+}
---
+++ my_calender 4/my_calender/obj/Debug/android/src/md5cbad293c55ad3d8a5525c823a0541a06/FourthActivity.java
@@ -0,0 +1,101 @@
+package md5cbad293c55ad3d8a5525c823a0541a06;
+
+
+public class FourthActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onSaveInstanceState:(Landroid/os/Bundle;)V:GetOnSaveInstanceState_Landroid_os_Bundle_Handler\n" +
+			"n_onPause:()V:GetOnPauseHandler\n" +
+			"n_onStop:()V:GetOnStopHandler\n" +
+			"n_onDestroy:()V:GetOnDestroyHandler\n" +
+			"n_onStart:()V:GetOnStartHandler\n" +
+			"n_onResume:()V:GetOnResumeHandler\n" +
+			"";
+		mono.android.Runtime.register ("my_calender.FourthActivity, my_calender", FourthActivity.class, __md_methods);
+	}
+
+
+	public FourthActivity ()
+	{
+		super ();
+		if (getClass () == FourthActivity.class)
+			mono.android.TypeManager.Activate ("my_calender.FourthActivity, my_calender", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onSaveInstanceState (android.os.Bundle p0)
+	{
+		n_onSaveInstanceState (p0);
+	}
+
+	private native void n_onSaveInstanceState (android.os.Bundle p0);
+
+
+	public void onPause ()
+	{
+		n_onPause ();
+	}
+
+	private native void n_onPause ();
+
+
+	public void onStop ()
+	{
+		n_onStop ();
+	}
+
+	private native void n_onStop ();
+
+
+	public void onDestroy ()
+	{
+		n_onDestroy ();
+	}
+
+	private native void n_onDestroy ();
+
+
+	public void onStart ()
+	{
+		n_onStart ();
+	}
+
+	private native void n_onStart ();
+
+
+	public void onResume ()
+	{
+		n_onResume ();
+	}
+
+	private native void n_onResume ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
